250cfcd8b8c833e5f8fe31bb2b134babdb639681
Esteban Santos Mendoza <eesantos.nqn@gmail.com>
2023-10-31 01:26:26 +0100

    Versión Final2 antes de las pruebas de funcionamiento

--- Archivos/Tres en Raya.py
+++ Archivos/Tres en Raya.py
@@ -236,4 +236,3 @@ No es tan difícil. Vuelva a intentarlo.")
 except KeyboardInterrupt:
     print("\n\nOk, no era necesario ser tan directo! Si no quiere seguir jugando no hay problema. Hasta luego.")
 
- 
